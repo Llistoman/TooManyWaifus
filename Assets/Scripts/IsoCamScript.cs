0d98e93cf4afe42647318dec1ccf580510bd4be3
Llistoman <josepll.plamarcell@gmail.com>
2017-12-08 12:49:48 +0100

    better camera controls and movement

--- Assets/Scripts/IsoCamScript.cs
+++ Assets/Scripts/IsoCamScript.cs
@@ -4,13 +4,48 @@ using UnityEngine;
 
 public class IsoCamScript : MonoBehaviour {
 
+    public GameObject target;
+    public float rotationDegrees;
+    public float time;
+
+    private bool allow;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        allow = true;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+        transform.position = target.transform.position;
+        
+        if(allow && (Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.Joystick1Button4)))
+        {
+            allow = false;
+            StartCoroutine(Rotation(new Vector3(0, rotationDegrees, 0), time));
+        }
+        else if (allow && (Input.GetKey(KeyCode.E) || Input.GetKey(KeyCode.Joystick1Button5)))
+        {
+            allow = false;
+            StartCoroutine(Rotation(new Vector3(0, -rotationDegrees, 0), time));
+        }
+    }
+
+    IEnumerator Rotation(Vector3 angle, float time)
+    {
+
+        Quaternion fromAngle = transform.rotation;
+        Quaternion toAngle = Quaternion.Euler(transform.eulerAngles + angle);
+        float elapsedTime = 0;
+        while (elapsedTime <= time)
+        {
+            elapsedTime += Time.deltaTime;
+            transform.rotation = Quaternion.Lerp(fromAngle, toAngle, (elapsedTime / time));
+            yield return new WaitForEndOfFrame();
+        }
+        allow = true;
+        yield return 0;
+    }
 }
